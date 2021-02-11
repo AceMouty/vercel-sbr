2b117300202e7fdb15c07fb8298105789f154cc6
Kyle Guerrero <kyleguerrero85@gmail.com>
2021-02-11 00:02:10 -0600

    chore: removed component in CTA section

--- src/components/Cta/index.jsx
+++ src/components/Cta/index.jsx
@@ -14,25 +14,23 @@ export default function Cta() {
 
     return (
         <Container maxW="6xl" paddingTop={isMobile ? "15rem" : "8rem"}>
-            <Box>
-                <Heading
-                    size={isMobile ? "4xl" : "3xl"}
-                    color={isMobile ? "whiteAlpha.800" : "whiteAlpha.900"}
-                    lineHeight="1.2"
-                    textAlign={!isMobile && "center"}
-                >
-                    Have A Glass<br />Share A Brew<br />Grab A Bite
+            <Heading
+                size={isMobile ? "4xl" : "3xl"}
+                color={isMobile ? "whiteAlpha.800" : "whiteAlpha.900"}
+                lineHeight="1.2"
+                textAlign={!isMobile && "center"}
+            >
+                Have A Glass<br />Share A Brew<br />Grab A Bite
                 </Heading>
-                <Box
-                    as="button"
-                    background="sbrPink"
-                    color="white"
-                    padding=".6rem 5rem"
-                    fontSize="2rem"
-                    marginTop="5rem"
-                    width={!isMobile && "100%"}
-                >Browse</Box>
-            </Box>
+            <Box
+                as="button"
+                background="sbrPink"
+                color="white"
+                padding=".6rem 5rem"
+                fontSize="2rem"
+                marginTop="5rem"
+                width={!isMobile && "100%"}
+            >Browse</Box>
         </Container>
     )
 }
