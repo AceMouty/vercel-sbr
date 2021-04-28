f52c9606ed559d9916402de921593cb2cff1ff48
Ace <ace@pop-os.localdomain>
2021-04-27 22:25:05 -0500

    chore(components): added new components to the page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,13 @@
 import { Layout } from "@components/Layout";
 import { Main } from "@components/Main";
 import { Features } from "@components/Features";
+import { Testimonials } from "@components/Testimonials";
 export default function Home() {
   return (
     <Layout>
       <Main />
       <Features />
+      <Testimonials />
     </Layout>
   );
 }
