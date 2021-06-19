35116dbb3017a148b1b77f2b0b804b9909647e9d
Kyle G <kyleguerrero85@gmail.com>
2021-06-18 20:29:25 -0500

    feat: created basic layout where the map will go

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,6 +1,22 @@
+import {
+  Box,
+  Center,
+  Container,
+  Heading,
+  Text
+} from "@chakra-ui/react";
 
-function Location() {
-    return <div>Location Component</div>
+function Features() {
+  return (
+    <Box as={"section"} pt={{ base: 20, md: 28 }}>
+      <Center>
+        <Heading as={"h2"} pb={{base: 20}}>Come Visit Us</Heading>
+      </Center>
+      <Container maxW={"5xl"} >
+          <Box maxw={"4xl"} backgroundColor={"tomato"} minHeight={"50vh"}/>
+      </Container>
+    </Box>
+  );
 }
 
-export default Location
+export default Features;
