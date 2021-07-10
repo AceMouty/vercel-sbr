3dc106772c3fe036acec214b5c59f8d6ac522f43
Kyle Guerrero <kyleguerrero85@gmail.com>
2021-07-09 22:31:01 -0500

    style(cta): fixed cta section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,46 +6,57 @@ import {
   Text,
   VStack,
   useBreakpointValue,
+  Container,
 } from "@chakra-ui/react";
 
 function Main() {
   return (
-    <Box as={"section"}>
-      <Flex
+    <Box
+      as={"section"}
+      w={"full"}
+      h={"100vh"}
+      backgroundImage={"linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(/showcase.jpg)"}
+      backgroundSize={"cover"}
+      backgroundPosition={"center right"}
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      {/* <Flex
         w={"full"}
         h={"100vh"}
-        backgroundImage={"url(/showcase.jpg)"}
+        backgroundImage={"linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url(/showcase.jpg)"}
         backgroundSize={"cover"}
         backgroundPosition={"center right"}
-      >
-        <VStack
+      > */}
+        {/* <VStack
           w={"full"}
           justify={"center"}
+          align={"flex-start"}
           px={useBreakpointValue({ base: 4, md: 8 })}
-          bgGradient={"linear(to-r, blackAlpha.600, blackAlpha.600)"}
-        >
-          <Stack maxW={"2xl"} align={"flex-start"} spacing={6}>
+          // bgGradient={"linear(to-r, blackAlpha.600, blackAlpha.600)"}
+        > */}
+          <Box maxW={{base: "15rem", md: "30rem"}}>
             <Text
               color={"white"}
               fontWeight={700}
               lineHeight={1.2}
-              fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
+              fontSize={useBreakpointValue({ base: "3xl", md: "6xl" })}
             >
               Have A Glass, Share A Brew And Grab A Bite
             </Text>
-            <Stack direction={"row"}>
-              <Button
-                bg={"pink.400"}
-                rounded={"full"}
-                color={"white"}
-                _hover={{ bg: "pink.500" }}
-              >
-                Learn More
-              </Button>
-            </Stack>
-          </Stack>
-        </VStack>
-      </Flex>
+            
+            <Button
+              bg={"pink.400"}
+              rounded={"full"}
+              color={"white"}
+              _hover={{ bg: "pink.500" }}
+            >
+              Learn More
+            </Button>
+          </Box>
+        {/* </VStack> */}
+      {/* </Flex> */}
     </Box>
   );
 }
